83aa3574ffa3508c2b6ca13b1755536b34e001d5
Lauany Reis da Silva <laulauany@hotmail.com>
2021-01-10 20:04:35 -0300

    Add files via upload

---
+++ mysite/views.py
@@ -0,0 +1,35 @@
+
+from django.shortcuts import render
+from django.http import JsonResponse
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+import requests
+
+def get_location_from_ip(ip_address):
+    response = requests.get("http://ip-api.com/json/{}".format(ip_address))
+    return response.json()
+
+def get_weather_from_ip(request):
+  ip_address = request.GET.get("ip")
+  location = get_location_from_ip(ip_address)
+  city = location.get("city")
+  country_code = location.get("countryCode")
+  weather_data = get_weather_from_location(city, country_code)
+  description = weather_data['weather'][0]['description']
+  temperature = weather_data['main']['temp']
+  s = "You're in {}, {}. You can expect {} with a temperature of {} degrees".format(city, country_code, description, temperature)
+  data = {"weather_data": s}
+  return JsonResponse(data)
+
+import os
+
+def get_weather_from_location(city, country_code):
+    token = os.environ.get("OPEN_WEATHER_TOKEN")
+    url = "https://api.openweathermap.org/data/2.5/weather?q={},{}&units=metric&appid={}".format(
+        city, country_code, token)
+    response = requests.get(url)
+    return response.json()
+
